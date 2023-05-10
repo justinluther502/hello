b6711ad85580acad897a5557ab41df13a78dec54
Justin Luther <justinluther@gmail.com>
2023-05-09 20:32:01 -0400

    refactor html files separate dir

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,8 @@ fn handle_connection(mut stream: TcpStream) {
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
-    let contents = fs::read_to_string(filename).unwrap();
+    let page_path = format!("pages/{filename}");
+    let contents = fs::read_to_string(page_path).unwrap();
     let length = contents.len();
     let response = format!(
         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
